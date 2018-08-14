df1b4e0c9922d9e20b3df34a9fb0a9ec0d548407
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-14 10:30:42 -0400

    Created store, added dispatcher, and created initial state

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,39 @@
-import { renderTodos } from './utils';
+import * as fromStore from "./store";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+import { renderTodos } from "./utils";
+
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const store = new fromStore.Store(
+  {},
+  {
+    todos: [{ label: "Eat pizza", complete: "false" }],
+  },
+);
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
-    if (!input.value.trim()) return;
+    if (!input.value.trim()) { return; }
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      payload,
+      type: "ADD_TODO",
+    });
 
-    input.value = '';
+    input.value = "";
   },
-  false
+  false,
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
   }
 });
